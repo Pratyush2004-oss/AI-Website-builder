1fbbb45b616a912ac0a4bba8b55aa173faea1e52
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-07-24 22:16:43 +0530

    Adding TRPC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
 
-const App = () => {
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import Client from "./client";
+
+const App = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: "Server Prefetch" })
+  );
   return (
-    <div>App</div>
-  )
-}
+  <HydrationBoundary state={dehydrate(queryClient)}>
+    <Suspense fallback={<div>Loading...</div>}>
+    <Client />
+    </Suspense>
+  </HydrationBoundary>
+  );
+};
 
-export default App
+export default App;
